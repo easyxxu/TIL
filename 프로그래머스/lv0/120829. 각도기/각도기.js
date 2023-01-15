dc2f448aefefba8b90cd07589749cd93fea36e88
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-15 23:31:42 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,3 +1,6 @@
-function solution(angle){
-	return [0,90,91,180].filter(x => angle >= x).length;
+function solution(angle) {
+    if(angle > 0 && angle < 90) return 1;
+    else if(angle === 90) return 2;
+    else if(angle > 90 && angle < 180) return 3;
+    else return 4;
 }

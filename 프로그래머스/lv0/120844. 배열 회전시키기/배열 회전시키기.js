5b5dc683a36373d06694d2af2e9d6354809b27f1
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-20 00:21:29 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,11 +1,4 @@
 function solution(numbers, direction) {
-    if(direction === "left"){
-        numbers.push(numbers[0])
-        numbers.shift()
-        return numbers
-    }else {
-        numbers.splice(0,0,numbers[numbers.length-1])
-        numbers.pop()
-        return numbers
-    }
+    "right" === direction ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
+    return numbers;
 }

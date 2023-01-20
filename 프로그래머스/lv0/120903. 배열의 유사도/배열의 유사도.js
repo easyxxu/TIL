b778605fe7ea9fe789b8af4409794a3fb8db8beb
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-20 14:00:02 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,10 +1,3 @@
 function solution(s1, s2) {
-    let array = []
-    for(let i=0;i<s1.length;i++){
-        for(let k=0;k<s2.length;k++){
-            if(s1[i]===s2[k]){
-                array.push(s1[i])
-            }
-        }
-    }return array.length
+    return s1.filter(x=>s2.includes(x)).length
 }

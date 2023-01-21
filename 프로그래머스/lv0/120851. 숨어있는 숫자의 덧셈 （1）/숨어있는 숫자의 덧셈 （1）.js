02e871cab861324d567c244890bbac3af49fc93f
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-21 11:56:17 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let string = my_string.split('')
+    let array = [...string].filter(x => parseInt(x))
+    let answer = 0
+    for(let k=0;k<array.length;k++){
+        answer += parseInt(array[k])
+    }
+    return answer
+}

c8b2e15e5b9ed5c586e31c3190867b0fe2c772b9
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-22 01:04:18 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,7 +1,10 @@
-function solution(cipher, code) {
-    let arr = []
-    for(let i=1;i<=cipher.length/code;i++){
-        arr.push(cipher.charAt(code * i -1))
-    }
-    return arr.join('')
+// function solution(cipher, code) {
+//     let arr = []
+//     for(let i=1;i<=cipher.length/code;i++){
+//         arr.push(cipher.charAt(code * i -1))
+//     }
+//     return arr.join('')
+// }
+function solution(cipher, code){
+	return cipher.split('').filter((_, index) => (index + 1) % code === 0).join('')
 }

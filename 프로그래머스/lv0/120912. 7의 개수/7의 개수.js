6fb4dc98b1913aa9bde75c61512fed169c3821db
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-27 17:48:31 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,9 +1,10 @@
 function solution(array) {
-    let arr = []
-    array.map(x => x.toString().split('')).forEach(a => {
-        a.forEach(b => {
-            if(b === '7')   arr.push(b)
-        })
-    })
-    return arr.length
+    // let arr = []
+    // array.map(x => x.toString().split('')).forEach(a => {
+    //     a.forEach(b => {
+    //         if(b === '7')   arr.push(b)
+    //     })
+    // })
+    // return arr.length
+    return array.join('').split('').filter(x=>x==='7').length
 }

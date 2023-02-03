410b1ba08084f2657a3f3e74451fd8bfc3f6bb14
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-02-03 11:29:45 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -2,14 +2,9 @@ function solution(sides) {
     let arr = [];
     let big = Math.max(...sides);
     let small = Math.min(...sides);
-    //x가 제일 긴 변인 경우
-    for(let x = big + 1; x < big + small; x++){
+    //big-small < x < big+small
+    for(let x = big - small + 1; x < big + small; x++){
         arr.push(x);
     }
-    //big이 제일 긴 변인 경우
-    for(let x = big-small+1; x <= big; x++){
-        arr.push(x);
-    }
-    console.log(arr)
     return arr.length;
 }

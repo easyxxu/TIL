736e87b03af143898c5beab61f624fc695a672fd
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-02-04 11:03:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,19 +2,17 @@ function solution(keyinput, board) {
     let coordinate = [0,0];
     const x = Math.floor(board[0] / 2); //x축 범위
     const y = Math.floor(board[1] / 2); //y축 범위
-    for(let i = 0; i<keyinput.length; i++){
-        if(keyinput[i] === "up"){
-            coordinate[1]++;
-            coordinate[1] > y ? coordinate[1]-- : null;
-        }else if(keyinput[i] === "down"){
-            coordinate[1]--;
-            coordinate[1] < -y ? coordinate[1]++ : null;
-        }else if(keyinput[i] === "left"){
-            coordinate[0]--;
-            coordinate[0] < -x ? coordinate[0]++ : null;
-        }else if(keyinput[i] === "right"){
-            coordinate[0]++;
-            coordinate[0] > x ? coordinate[0]-- : null;
+    
+    for(let i of keyinput){
+        switch(i){
+            case 'left': 
+                if(-coordinate[0] < x) coordinate[0]--; break;
+            case 'right':
+                if(coordinate[0] < x) coordinate[0]++; break;
+            case 'up':
+                if(coordinate[1] < y) coordinate[1]++; break;
+            case 'down':
+                if(-coordinate[1] < y) coordinate[1]--; break;
         }
     }
     return coordinate;

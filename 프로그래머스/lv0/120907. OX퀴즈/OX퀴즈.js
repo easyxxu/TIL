a8f03ed48504a9d984e4275e94382d68892fc8d4
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-02-17 21:36:00 +0900

    [level 0] Title: OX퀴즈, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,32 +1,18 @@
 function solution(quiz) {
-    let ans = [];
-    
-    quiz.forEach((item) => {
-        const oneQuiz = item.split(" ");
-        
-        const X = Number(oneQuiz[0]);
-        const Y = Number(oneQuiz[2]);
-        
-        let calc = 0;
-        
-        if(oneQuiz[1] === "+"){
-            calc = X + Y;
-        }
-        
-        if(oneQuiz[1] === "-"){
-            calc = X - Y;
-        }
-        
-        const result = Number(oneQuiz[4]);
-        
-        if(calc === result){
-            ans.push("O");
-        }
-        
-        if(calc !== result){
-            ans.push("X");
-        }
-    })
-    
-    return ans;
+    let xo = [];
+    let result;
+    for(let i of quiz){
+        const arr = i.split(' ');
+        arr.forEach(item => {
+            if(item === '-'){
+                result = parseInt(arr[0],10) - parseInt(arr[2],10);
+                result === parseInt(arr[4]) ? xo.push('O') : xo.push('X');
+            }
+            if(item === '+'){
+                result = parseInt(arr[0], 10) + parseInt(arr[2], 10);
+                result === parseInt(arr[4]) ? xo.push('O') : xo.push('X');
+            }
+        })
+    }
+    return xo
 }

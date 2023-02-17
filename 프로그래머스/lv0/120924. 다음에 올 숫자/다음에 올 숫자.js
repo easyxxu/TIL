68e961dd2d168f0bc9d8b2e85dd345d015472f36
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-02-17 22:22:03 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,9 @@
 function solution(common) {
-    const sameMinus = common[1]-common[0] === common[2]-common[1]
-    if(sameMinus) {
-        return common[common.length-1]+common[1]-common[0]
-    } else {
-        return common[common.length-1]*(common[1]/common[0])
+    //등차수열
+    if(common[0]-common[1] == common[1] - common[2]){
+        return common[common.length-1] + common[1]-common[0]
     }
+    //등비수열
+    else 
+        return common[common.length-1] * common[1] / common[0]
 }

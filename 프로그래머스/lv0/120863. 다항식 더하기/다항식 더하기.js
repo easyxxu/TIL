b8ccf5d401e59ae2fe496405baead42ff98e9708
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-02-17 16:00:10 +0900

    [level 0] Title: 다항식 더하기, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,49 +1,16 @@
 function solution(polynomial) {
-    const polyArr = polynomial.split(" + ");
-    
-    let xNum = 0;//x끼리
-    let num = 0;//상수끼리
-    
-    polyArr.forEach((item) => {
-        //x끼리 연산
-        if(item.includes("x")){
-            const xArr = item.split("x");
-            console.log(xArr)
-            if(xArr[0] === ""){
-                xNum += 1;
-            }
-            //1x인 경우
-            if(xArr[0] !== ""){
-                xNum += Number(xArr[0]);
-            }
-        }
-        //상수끼리 연산
-        if(!item.includes("x")){
-            num += Number(item);
-        }
-    })
-    //xNum과 num이 0이 아닌 경우 => x항과 상수항이 둘 다 존재
-    if(xNum !== 0 && num !== 0){
-        if(xNum === 1){//x의 계수가 1인 경우
-            return `x + ${num}`;        
-        }
-        
-        return `${xNum}x + ${num}`;    
-    }
-    //xNum은 0이 아니면서 num은 0인 경우 => x항만 존재
-    if(xNum !== 0 && num === 0){
-        if(xNum === 1){//x의 계수가 1인 경우
-            return "x";
-        }
-        
-        return `${xNum}x`;
-    }
-    //상수항만 존재하는 경우
-    if(xNum === 0 && num !== 0){
-        return `${num}`;
-    }
-    //x항과 상수항이 존재하지 않는 경우
-    if(xNum === 0 && num === 0){
-        return "0";
-    }
+    const arr = polynomial.split(" + ");
+    const xNum = arr
+                .filter(n => n.includes("x"))
+                .map(n => n.replace('x', '') || '1')
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+    const num = arr
+                .filter(n => !isNaN(n))
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+
+    let answer = [];
+    if(xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+    if(num) answer.push(num);
+
+    return answer.join(" + ");
 }

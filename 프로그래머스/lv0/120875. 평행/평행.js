d3025bb1f6953d2601e60323eddf30042088b295
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-01 22:39:26 +0900

    [level 0] Title: 평행, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -0,0 +1,24 @@
+function solution(dots) {
+    const a = dots[0]
+    const b = dots[1]
+    const c = dots[2]
+    const d = dots[3]
+    
+    // 직선 ab 기울기, 직선 cd 기울기
+    const ab = Math.abs(b[1]-a[1]) / Math.abs(b[0]-a[0])
+    const cd = Math.abs(c[1]-d[1]) / Math.abs(c[0]-d[0])
+    
+    // 직선 ac 기울기, 직선 bd 기울기
+    const ac = Math.abs(a[1]-c[1]) / Math.abs(a[0]-c[0])
+    const bd = Math.abs(b[1]-d[1]) / Math.abs(b[0]-d[0])
+    
+    // 직선 ad 기울기, 직선 bc 기울기
+    const ad = Math.abs(a[1]-d[1]) / Math.abs(a[0]-d[0])
+    const bc = Math.abs(b[1]-c[1]) / Math.abs(b[0]-c[0])
+    
+    //기울기가 같다면 평행
+    if(ab === cd || ac === bd || ad === bc) 
+        return 1
+    else return 0
+    
+}

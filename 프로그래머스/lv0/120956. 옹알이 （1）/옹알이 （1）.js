a501d7de76058e968d798ff49f2577b728a3625c
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-02 17:14:23 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,14 @@
+function solution(babbling) {
+    var answer = 0;
+    for (var i = 0 ; i < babbling.length ; i++ ) {
+        if(babbling[i]
+           .replace("ye"," ")
+           .replace("aya"," ")
+		   .replace("woo"," ")
+           .replace("ma"," ")
+           .trim() == ""){
+            answer = answer + 1 ;
+        }    
+    } 
+    return answer;
+}

04d02e5a015c01f088a9ca11ba7578a624a18f43
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-03 17:12:21 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    let num = []
+    let sum = 0
     for(let i=0;i<=n;i++){
         if(n % i === 0){
-            num.push(i)
+            sum += i
         }
     }
-    return num.reduce((a,b)=>a+b, 0)
+    return sum
 }

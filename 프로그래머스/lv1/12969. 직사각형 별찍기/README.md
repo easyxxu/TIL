0668a45e078c315a0fb74af5bf849f964e5b2fc9
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-16 14:13:01 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.46 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 69.03 ms
+메모리: 32.3 MB, 시간: 43.46 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,11 +3,8 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
     
-    for(let i=0;i<b;i++){
-        let star = ""
-        for(let k=0;k<a;k++){
-            star += '*'
-        }console.log(star)
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
     }
-    
 });

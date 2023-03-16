9c15a43a21080c32b58e7b8a0aecbc9a767eaf7c
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-16 22:57:33 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -3,6 +3,7 @@ function solution(s) {
     let result = []
     for(let i=0; i<arr.length;i++){
         let string = arr[i].split('')
+        //s에서 공백을 기준으로 잘라 각 단어의 대소문자 변경
         for(let k=0; k<string.length; k++){
             if(k % 2 == 0){
                 string[k] = string[k].toUpperCase()
@@ -11,8 +12,10 @@ function solution(s) {
             }    
             result.push(string[k])
         }
+        //단어를 구분하기 위한 공백 푸쉬
         result.push(' ')
     }
+    //마지막에 공백이 들어가기 때문에 pop을 이용해 빼줌
     result.pop()
     return result.join('')
 }

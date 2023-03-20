13c4e12e48214a9f9b6f8865be37a72751ad9902
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-03-20 23:02:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,4 +1,21 @@
 function solution(n) {
     let reversed_N = n.toString(3).split('').reverse().join('')
     return parseInt(reversed_N, 3)
+    // 런타임 에러나는 내 코드.. toString을 기억하지 못한 사람의 최후..
+    // let arr = []
+    // let result = []
+    // //3진법-뒤집힌 상태
+    // while(n !== 1){
+    //     arr.push(n % 3)
+    //     n = Math.floor(n / 3)
+    // }
+    // arr.push(n)
+    // console.log(arr)
+    // let i = arr.length - 1
+    // //10진법으로 변경
+    // arr.forEach(x => {
+    //     result.push(x * (3 ** i))
+    //     i--
+    // })
+    // return result.reduce((a,b) => a + b)
 }

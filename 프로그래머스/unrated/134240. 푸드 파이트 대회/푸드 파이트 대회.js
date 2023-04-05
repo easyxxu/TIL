e8bcdfd28447af0007427dd744b8c176496c97b4
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-05 17:39:38 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,11 @@
 function solution(food) {
     let player = []
     let dish 
-    let result
     for(let i = 1;i < food.length; i++){
         dish = parseInt(food[i] / 2)
         for(let k = 0;k < dish; k++){
             player.push(i)
         }
     }
-    result = player.join('') + 0 + player.reverse().join('')
-    return result
+    return player.join('') + 0 + player.reverse().join('')
 }

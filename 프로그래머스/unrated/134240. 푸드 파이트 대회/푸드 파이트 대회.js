430a01103775336e27631009b9f7caa049415f09
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-05 17:37:13 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,13 @@
 function solution(food) {
-    let playerA = []
+    let player = []
     let dish 
+    let result
     for(let i = 1;i < food.length; i++){
         dish = parseInt(food[i] / 2)
         for(let k = 0;k < dish; k++){
-            playerA.push(i)
+            player.push(i)
         }
     }
-    //reverse()는 원본 배열을 변경하기 때문에 전개연산자를 이용하여 원본 배열을 유지함
-    let playerB = [...playerA].reverse()
-    return [...playerA, 0, ...playerB].join('')
+    result = player.join('') + 0 + player.reverse().join('')
+    return result
 }

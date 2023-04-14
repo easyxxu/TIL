c63e2962cda1282a701bfd1c89fd9a61fe1c7d5d
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-14 21:44:16 +0900

    [unrated] Title: 추억 점수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,6 @@
+function solution(name, yearning, photo) {
+    return photo.map(v => {
+      return v.filter(x => x = name.includes(x)).map(v => yearning[name.indexOf(v)]).reduce((acc, cur) => acc + cur,0)
+    });
+    
+}

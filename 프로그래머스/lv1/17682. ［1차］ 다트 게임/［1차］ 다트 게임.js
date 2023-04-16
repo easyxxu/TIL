334cabccf076f7a6b7188c0aad58900e21d7f28a
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-16 20:12:25 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,45 @@
+function solution(dartResult) {
+  let answer = 0;
+  let point = [];
+  let idx = -1;
+  let resultArr = dartResult.replace(/10/g, "k").split("");
+
+  for (let i = 0; i < resultArr.length; i++) {
+    const temp = resultArr[i] === "k" ? "10" : resultArr[i];
+    temp;
+
+    if (isNaN(temp)) {
+      switch (temp) {
+        case "S":
+          point[idx] = point[idx];
+          break;
+        case "D":
+          point[idx] = point[idx] ** 2;
+          break;
+        case "T":
+          point[idx] = point[idx] ** 3;
+          break;
+        case "*":
+          if (idx > 0) {
+            point[idx] *= 2;
+            point[idx - 1] *= 2;
+          } else {
+            point[idx] *= 2;
+          }
+          break;
+        case "#":
+          point[idx] *= -1;
+          break;
+      }
+    } else {
+      idx += 1;
+      point[idx] = +temp;
+    }
+  }
+
+  answer = point.reduce((sum, currValue) => {
+    return sum + currValue;
+  }, 0);
+
+  return answer;
+}

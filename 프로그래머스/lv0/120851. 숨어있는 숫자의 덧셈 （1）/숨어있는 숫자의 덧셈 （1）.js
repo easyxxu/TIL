ecacb5376684f6fb694fd545cf443214d5d027a2
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-17 11:23:18 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,5 @@
 function solution(my_string) {
-    let string = my_string.split('')
-    let array = string.filter(x => parseInt(x))
+    let array = my_string.split('').filter(x => parseInt(x))
     let answer = 0
     for(let k=0;k<array.length;k++){
         answer += parseInt(array[k])

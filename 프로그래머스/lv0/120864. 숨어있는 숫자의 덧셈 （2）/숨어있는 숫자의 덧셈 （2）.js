cd9db56174ccc20afa142bb33677868c78c70a44
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-04-17 13:33:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,13 +1,13 @@
 function solution(my_string) {
-    let answer=0
-    for(let i=0;i<my_string.length;i++){
-        let number=''
-        while(!Number.isNaN(Number(my_string[i]))){
-            number+=my_string[i]
-            i++
-        }
-        answer += +number
-    }
-    return answer
-    // return my_string.match(/[0-9]+/g).reduce((a,b) => parseInt(a) + parseInt(b),0)
+    // let answer=0
+    // for(let i=0;i<my_string.length;i++){
+    //     let number=''
+    //     while(!Number.isNaN(Number(my_string[i]))){
+    //         number+=my_string[i]
+    //         i++
+    //     }
+    //     answer += +number
+    // }
+    // return answer
+    return (my_string.match(/[0-9]+/g)??[]).reduce((a,b) => parseInt(a) + parseInt(b),0)
 }

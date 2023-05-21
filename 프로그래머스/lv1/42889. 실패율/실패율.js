aa1e96a108a63722b2fb5f132e490ca8a13557fa
jisu lee <107910342+easyxxu@users.noreply.github.com>
2023-05-21 22:04:38 +0900

    [level 1] Title: 실패율, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,26 +1,11 @@
 function solution(N, stages) {
-    //실패율 구하기
-    let fail = []
-    for(let i=1;i<=N;i++){
-        let yet = 0
-        let success = 0
-        for(let k = 0;k < stages.length;k++){
-            if(stages[k] == i){
-                yet++
-            }else if(stages[k] >= i+1){
-                success++
-            }
-        }
-        fail.push({stage:i, percent: yet/success})
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
     }
-    //내림차순
-    fail.sort((a,b)=>{
-        if(a.percent === b.percent){
-            return a.stage - b.stage
-        }
-        else{
-            return b.percent - a.percent
-        }
-    })
-    return fail.map(x => x.stage)
+    
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }

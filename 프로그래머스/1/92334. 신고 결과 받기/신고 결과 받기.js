8fb585ad5f1aadb7c35f31dd142fbd1e29ecfad3
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-01-02 10:06:24 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,28 +1,32 @@
 function solution(id_list, report, k) {
     const set = new Set(report);
-    const reportedUserArr = [];
-    const finalReportedUsers = new Array(id_list.length).fill(0);
-    const reportedUsers = [];
+    const reportedUsersID = [];
+    const reportedUsersCnt = new Array(id_list.length).fill(0);
+    const finalReportedUsers = [];
     const result = new Array(id_list.length).fill(0);
     
+    // 중복을 없앤 신고당한 유저의 ID List
     for(let i of set.values()){
     	const reportArr = i.split(' ');
-        reportedUserArr.push(reportArr[1]);
+        reportedUsersID.push(reportArr[1]);
     }
+	// 유저가 신고받은 횟수
     for(let i=0; i < id_list.length; i++){
-        reportedUserArr.forEach(user => {
+        reportedUsersID.forEach(user => {
             if(user === id_list[i])
-                finalReportedUsers[i]++;
+                reportedUsersCnt[i]++;
         })
     }
+    // 최종적으로, 신고받은 횟수 >= k 인 유저 ID List
     for(let i=0;i<id_list.length;i++){
-        if(finalReportedUsers[i] >= k){
-            reportedUsers.push(id_list[i]);
+        if(reportedUsersCnt[i] >= k){
+            finalReportedUsers.push(id_list[i]);
         }
     }
+    // 유저 처리 결과 메일 받은 횟수
     for(let i of set.values()){
         const reportArr = i.split(' ');
-        for(let k of reportedUsers){
+        for(let k of finalReportedUsers){
             if(k === reportArr[1]){
                 id_list.forEach((id,idx) => id === reportArr[0] ? result[idx]++ : null);
             }

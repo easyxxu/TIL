c97cba188bc8702355dcd4f69ff84a72b1a2f6ac
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-02-19 15:38:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1368.58 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,10 @@
+function solution(elements) {
+    const set = new Set;
+    for(let i=1;i<=elements.length;i++){
+        const arr = elements.concat(elements.slice(0,i));
+        for(let k=0;k<arr.length;k++){
+            set.add(arr.slice(k,k+i).reduce((a,b) => a+b))
+        }
+    }
+    return set.size
+}

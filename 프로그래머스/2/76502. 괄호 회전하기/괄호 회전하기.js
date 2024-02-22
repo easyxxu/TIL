7a6d192b5a8835cd4276e98ea5cebe136b3570df
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-02-22 23:03:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,16 +1,14 @@
 function solution(s) {
-    let result = 0;
-    if (s.length % 2 == !0) return 0;
-    for(let i=0;i<s.length;i++){
-        const arr = [];
-        const tmp = i === 0 ? s : s.slice(i) + s.slice(0,i); // 회전
-        for(let j=0;j<tmp.length;j++){
-            if(arr[arr.length - 1] === '(' && tmp[j] === ')') arr.pop();
-            else if(arr[arr.length - 1] === '[' && tmp[j] === ']') arr.pop();
-            else if(arr[arr.length - 1] === '{' && tmp[j] === '}') arr.pop();
-            else arr.push(tmp[j]);
+    let result = 0
+    const stack = s.split('')
+        
+    for(let i=0;i<stack.length;i++){
+        if(i === 0 && (stack[i] === ']' || stack[i] === ')' || stack[i] === '}')){
+            let item = stack.splice(0,1)
+            stack.push(item[0])
+            console.log(stack)
+            return
         }
-        if(arr.length === 0) result++;
     }
-    return result;
+    
 }

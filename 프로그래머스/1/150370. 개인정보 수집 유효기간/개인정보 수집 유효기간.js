3dadd9ddb26b09e75b6546242e28dd171eb26c39
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-03-18 13:20:54 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,32 +1,20 @@
 function solution(today, terms, privacies) {
     const result = []
-    today = today.split('.')
-    const todayY = parseInt(today[0])
-    const todayM = parseInt(today[1])
-    const todayD = parseInt(today[2])
-    const todayDate = todayY * 336 + todayM * 28 + todayD
+	const [year, month, day] = today.split('.').map(Number);
+    const todayDate = year * 336  + month * 28 + day
+
+    const termsObj = {}
+    terms.map(term => {
+        const [type, validPeriod] = term.split(' ');
+        termsObj[type] = Number(validPeriod);
+    })
     
     privacies.forEach((privacy, idx) => {
-		privacy = privacy.split(' ')
-        let agree = privacy[0]
-        const kind = privacy[1]
-
-        agree = agree.split('.')
-        const agreeY = parseInt(agree[0])
-        const agreeM = parseInt(agree[1])
-        const agreeD = parseInt(agree[2])
+		const [agreeDate, agreeType] = privacy.split(' ')
+        const [agreeY, agreeM, agreeD] = agreeDate.split('.').map(Number);
+        const date = agreeY * 336 + agreeM * 28 + agreeD+ termsObj[agreeType] * 28
         
-        let validPeriod 
-        terms.forEach((term,i) => {
-            const termArr = term.split(' ')
-            const termKind = termArr[0]
-            const period = termArr[1]
-            if(termKind === kind) validPeriod = period
-        })
-		
-        const agreeDate = agreeY * 336 + agreeM * 28 + agreeD+ validPeriod * 28
-        
-		if(agreeDate <= todayDate) result.push(idx+1)
+		if(date <= todayDate) result.push(idx+1)
     })
     return result
 }

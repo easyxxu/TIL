c9e0e07d75b828e3a133f2476e6d138ec8520aea
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-06-27 10:50:01 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,33 +1,27 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
 function solution(want, number, discount) {
     let count = 0;
-    for (let i = 0; i < discount.length - 9; i++) {
-        const slice = discount.slice(i, i+10);
+    const lastDay = discount.length - 9;
 
+    for (let i = 0; i < lastDay; i++) {
+        const slice = discount.slice(i, i + 10);
+        const sliceCount = {};
+
+        // slice 배열의 각 항목의 개수를 세기
+        for (let item of slice) {
+            sliceCount[item] = (sliceCount[item] || 0) + 1;
+        }
+
+        // want 배열과 비교
         let flag = true;
         for (let j = 0; j < want.length; j++) {
-            if (slice.filter(item => item === want[j]).length !== number[j]) {
+            if (sliceCount[want[j]] !== number[j]) {
                 flag = false;
                 break;
             }
         }
+
         if (flag) count += 1;
     }
+
     return count;
 }

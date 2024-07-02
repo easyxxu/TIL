a452151f1285e77efd6b6dcef3efaac9733dde25
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-07-02 09:43:06 +0900

    [level 2] Title: 의상, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,6 +1,6 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 02일 09:42:35
+2024년 07월 02일 09:43:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,6 @@
 function solution(clothes) {
-    const clothesMap = {}
-    let answer = 1
-    // 의상 종류별로 개수를 저장
-    clothes.forEach(arr => {
-        const [type, name] = arr
-        if(clothesMap.hasOwnProperty(name)){
-            clothesMap[name]++
-        }else{
-            clothesMap[name] = 1
-        }
-    })
-
-    // 조합 구하기
-    for(const key in clothesMap){
-        answer *= (clothesMap[key] + 1)
-    }
+    // 모든 조합을 구한 다음에 겹치는 종류는 빼기
+    let combineCnt = clothes.length
     
-    return answer - 1
+    
 }

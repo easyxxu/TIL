a78523422eedb352da45858939eb2b1214b55faa
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-07-10 09:28:18 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -3,6 +3,6 @@ const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const remainder = input.map((x) => x % 42);
-const result = new Set();
-remainder.forEach((x) => result.add(x));
+const result = new Set(remainder);
+
 console.log(result.size);

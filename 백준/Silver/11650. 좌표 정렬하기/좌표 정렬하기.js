5b9ffa89371e84cb426a7307ef6839721559fc1e
jisu lee <107910342+easyxxu@users.noreply.github.com>
2024-10-07 16:26:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 396 ms, Memory: 59452 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const spot = input.slice(1).map((x) => x.split(" ").map(Number));
+
+spot.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
+});
+
+let answer = "";
+for (let i = 0; i < spot.length; i++) {
+  answer += spot[i][0] + " " + spot[i][1] + "\n";
+}
+console.log(answer);
